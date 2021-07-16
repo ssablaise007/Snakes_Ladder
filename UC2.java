f40b3334b21dd302d28804ceb313bd4f2a1e022b
Your Name <you@example.com>
2021-07-16 08:23:24 +0530

    Added : Roll the dice to get Number

---
+++ UC2.java
@@ -0,0 +1,11 @@
+package SnakesLadder;
+
+import java.util.Random;
+
+public class UC2 {
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int dice  = rand.nextInt(6);
+	System.out.println("State of Dice :"+dice);
+	}
+}
